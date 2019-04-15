76cfce764395f1bb5a265c5857000bc146b0a44b
Sean Doyle <sdoylelambda@gmail.com>
2019-04-15 15:46:44 -0400

    App.js routes declared.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
@@ -6,13 +7,19 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <Link className="home-link" to="/">
+             Home
+          </Link>
         </div>
         <div>
-          <a href="">About</a>
+           <Link className="about-link" to="/about">
+            About
+           </Link>
         </div>
         <div>
-          <a href="">Contact</a>
+          <Link className="contact-link" to="/contact">
+          Contact
+          </Link>
         </div>
       </div>
     </div>
